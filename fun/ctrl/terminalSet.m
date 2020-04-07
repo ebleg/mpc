f6ec0ecdd8c2b7e9c41218af855f0f9df4dd64db
esalzmann <esalzmann@users.noreply.github.com>
2020-04-07 13:12:53 +0200

    terminal Set

--- fun/ctrl/terminalSet.m
+++ fun/ctrl/terminalSet.m
@@ -11,21 +11,6 @@ end
 dim = par.angCtrl.dim;
 
 % admisseble set: T * attitude + S *u_N
-
-
-
-
-
-
-
-
-xref = reshape(xref, [(par.posCtrl.dim.N+1)*par.posCtrl.dim.x, 1]);
-Tf = T((end-par.posCtrl.dim.x+1):end,:);
-Sf = S((end-par.posCtrl.dim.x+1):end,:);
-xf = xref((end-par.posCtrl.dim.x+1):end);
-Psqrt = chol(par.posCtrl.P);
-    
-norm(Psqrt*(Tf*pos + Sf*u_N - xf)) <= par.posCtrl.Xf;
-    
+   
 
 end
